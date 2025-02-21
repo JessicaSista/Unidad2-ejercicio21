27b4c93c55aced1932abdea34fbb5d20b421061a
JessicaSista <sistajessica2@gmail.com>
2025-02-21 15:32:35 -0300

    Seeders listos

--- seeders/tweetSeeder.js
+++ seeders/tweetSeeder.js
@@ -14,18 +14,28 @@
  */
 
 const faker = require("@faker-js/faker").fakerES;
-const Article = require("../models/Article");
+const Tweet = require("../models/Tweet");
+const User = require("../models/User");
 
 module.exports = async () => {
-  const articles = [];
+  const tweets = [];
+  const users = await User.find();
 
   for (let i = 0; i < 500; i++) {
-    articles.push({
-      title: faker.lorem.sentence(5),
-      content: faker.lorem.paragraphs(),
+    const randomUser = users[faker.number.int({ min: 0, max: users.length - 1 })];
+
+    const newTweet = new Tweet({
+      text: faker.lorem.sentence(20).substring(0, 140), // Asegura máximo 140 caracteres
+      user: randomUser._id,
+      createdAt: faker.date.past(),
+      likes: faker.datatype.number({ min: 0, max: 1000 }),
     });
+    tweets.push(newTweet);
+
+    randomUser.tweetList.push(newTweet._id);
+    await randomUser.save();
   }
 
-  await Article.insertMany(articles);
-  console.log("[Database] Se corrió el seeder de Articles.");
+  await Tweet.insertMany(tweets);
+  console.log("[Database] Se corrió el seeder de Tweets.");
 };
