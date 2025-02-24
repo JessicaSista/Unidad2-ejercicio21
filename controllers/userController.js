aadee3e010ac2a8f563e46f426cdb7dd9b5e4218
JessicaSista <sistajessica2@gmail.com>
2025-02-24 13:21:35 -0300

    Seguir / dejar de seguir

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,27 @@ async function store(req, res) {
 }
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  const id = req.params.id;
+  const toFollow = await User.findById(id); //encuentro el usuario a seguir (o no)
+
+  // const user = req.auth;
+  // user no existe pero sería la persona que está autenticada, lo haríamos con el middelware
+
+  if (user.following.includes(toFollow.username)) {
+    //si lo seguía
+    user.following.pull(toFollow.username); //saco a ese usuario como seguidor
+    toFollow.followers.pull(user.username); //me saco como seguidor de ese usuario
+  } else {
+    //si no lo seguía
+    user.following.push(toFollow.username); //agrego a ese usuario como seguidor
+    toFollow.followers.push(user.username); //me agrego como seguidor de ese usuario
+  }
+
+  //guardo los cambios en la base de datos
+  await user.save();
+  await toFollow.save();
+}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {}
