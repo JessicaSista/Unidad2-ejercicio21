6051bab0e48802df765f3acf8c2f92c36dee819e
Diego Abad <diegoabamontevideo@gmail.com>
2025-02-25 10:20:05 -0300

    orden en index routes corregido para que no se bloquee

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,11 @@ module.exports = (app) => {
    * (en inglés y en plural). Del mismo modo, las rutas relativas a los artículos
    * se deberían agrupar bajo la URL `/articles` (en inglés y en plural).
    */
-  app.use("/users", userRoutes);
-  app.use("/articles", articleRoutes);
+
   app.use("/", authRoutes);
+  app.use("", userRoutes);
+  app.use("/articles", articleRoutes);
   app.use("/tweets", tweetRoutes);
 };
+
+
