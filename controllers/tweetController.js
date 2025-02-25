c912e89a44dd85215e57ef21a7e273809ddee463
Diego Abad <diegoabamontevideo@gmail.com>
2025-02-25 20:15:42 -0300

    Tweet controller: no tweets vacíos + try/catch

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -12,24 +12,40 @@ async function show(req, res) {}
 async function store(req, res) {
   const { text } = req.body;
   const userId = req.auth.sub;
-  const newTweet = await Tweet.create({ text, user: userId });
-  res.json(newTweet);
+  if (!text.length) {
+    return res.json("El campo text no puede estar vacío");
+  }
+
+  try {
+    const newTweet = await Tweet.create({ text, user: userId });
+    return res.json(newTweet);
+  } catch (error) {
+    return res.status(500).json({ message: "Hubo un error creado el tweet", error: error.message });
+  }
 }
 
 // Update the specified resource in storage.
 async function update(req, res) {
-  const tweet = await Tweet.findById(req.params.id);
-  const userId = req.auth.sub;
-  tweet.likes.includes(userId) ? tweet.likes.pull(userId) : tweet.likes.push(userId);
-  await tweet.save();
-  res.json(tweet);
+  try {
+    const tweet = await Tweet.findById(req.params.id);
+    const userId = req.auth.sub;
+    tweet.likes.includes(userId) ? tweet.likes.pull(userId) : tweet.likes.push(userId);
+    await tweet.save();
+    res.json(tweet);
+  } catch (error) {
+    res.status(500).json({ message: "Hubo un error actualizando los likes", error: error.message });
+  }
 }
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
-  const tweet = await Tweet.findById(req.params.id);
-  await tweet.deleteOne();
-  res.json({ message: "El tweet se ha eliminado correctamente" });
+  try {
+    const tweet = await Tweet.findById(req.params.id);
+    await tweet.deleteOne();
+    res.json({ message: "El tweet se ha eliminado correctamente" });
+  } catch (error) {
+    res.status(500).json({ message: "Hubo un error eliminando el tweet", error: error.message });
+  }
 }
 
 // Otros handlers...
